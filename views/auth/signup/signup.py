dbc8e5fdbd00abb29a44dba4f135acb650b58656
RaianyMendesIF <raianymendes@estudante.ifms.edu.br>
2026-05-14 22:04:40 -0400

    teste de conexao com o banco de dados com o neon

--- views/auth/signup/signup.py
+++ views/auth/signup/signup.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends
-from dependencies.dependencies import pegar_sessao
+from fastapi import APIRouter, Depends, HTTPException
+from database import pegar_sessao
 from models.model import Usuario
 from schemas.schemas import Usuario_schema_cadastro
 from sqlalchemy.orm import Session
+from sqlalchemy import text
 
 router = APIRouter(prefix="/auth", tags=["Autentificacao"])
 
@@ -28,3 +29,13 @@ async def criar_conta(dados: Usuario_schema_cadastro, session: Session = Depends
         session.rollback()
         print(f"ERRO DE BANCO: {e}")
         return {"status": 500, "detalhe_tecnico": str(e)}
+
+
+@router.get("/test-db")
+async def validar_conexao(session: Session = Depends(pegar_sessao)):
+    try:
+        # Agora o 'text' está importado e vai funcionar
+        session.execute(text("SELECT 1"))
+        return {"status": "sucesso", "mensagem": "Conectado ao Neon!"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro real: {str(e)}")
